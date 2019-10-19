69493f02aa4e73de1c733fba573b92a0eeee1528
Nikhil KR <nitnik.22@gmail.com>
2019-10-19 22:02:56 +0530

    added coordFinder and requirements.txt

--- coordFinder.py
+++ coordFinder.py
@@ -1,1 +1,22 @@
 import pyautogui
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    #print('{0} pressed'.format(key))
+    pass
+
+
+def on_release(key):
+    #print('{0} release'.format(key))
+
+    if str(key) == "'p'":
+        print(pyautogui.position())
+
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
